823dce773692ea5d491c1501a6e5e6bef2a9dc15
jsampedro98 <jsampedropro@gmail.com>
2024-07-17 04:01:11 +0200

    Ajout de la page Nous Rencontrer, Mise en Place du Hook Admin

--- app/public/wp-content/themes/planty/functions.php
+++ app/public/wp-content/themes/planty/functions.php
@@ -11,3 +11,24 @@ function theme_enqueue_styles()
         filemtime(get_stylesheet_directory() . '/css/theme.css')
     );
 }
+
+
+function add_admin_link_to_menu($items, $args)
+{
+
+    if (is_user_logged_in() && current_user_can('administrator')) {
+
+        $admin_link = '<li><a href="' . admin_url() . '">Admin</a></li>';
+
+
+        $needle = 'Nous rencontrer';
+        $position = strpos($items, $needle);
+
+        if ($position !== false) {
+            $position += strlen($needle);
+            $items = substr_replace($items, $admin_link, $position, 0);
+        }
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2);
